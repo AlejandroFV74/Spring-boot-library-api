6b19e3a5a1f7fa55c49e5bf97fbd80af8a2366b0
AlejandroFV74 <00067323@uca.edu.sv>
2026-04-02 23:38:05 -0600

    Add new book entity

---
+++ library-api/src/main/java/com/flores/libraryapi/entities/Book.java
@@ -0,0 +1,57 @@
+package com.flores.libraryapi.entities;
+
+public class Book {
+    private long id;
+    private String title;
+    private String author;
+    private double price;
+    private String isbn;
+
+    public Book(long id, String title, String author, double price, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isbn = isbn;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
