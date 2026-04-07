a6d8b1eea64544e3ca6e8640ee74c7eab0614bdc
AlejandroFV74 <00067323@uca.edu.sv>
2026-04-07 17:16:13 -0600

    Basic CRUD for book entity

--- library-api/src/main/java/com/flores/libraryapi/controllers/BookController.java
+++ library-api/src/main/java/com/flores/libraryapi/controllers/BookController.java
@@ -20,8 +20,40 @@ public class BookController {
         return bookRepository.findAll();
     }
 
+    @GetMapping(params = "author")
+    public List<Book> findByAuthor(@RequestParam String author) {
+        return bookRepository.findByAuthor(author);
+    }
+
+    @GetMapping(params = "id")
+    public Book findByID(@RequestParam long id) {
+        return bookRepository.findById(id);
+    }
+
+    @GetMapping(params = "title")
+    public List<Book> findByTitle(@RequestParam String title) {
+        return bookRepository.findByTitle(title);
+    }
+
     @PostMapping
     public Book save(@RequestBody Book book) {
         return bookRepository.save(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book update(@PathVariable long id, @RequestBody Book updatedBook) {
+        Book book = bookRepository.findById(id);
+
+        book.setTitle(updatedBook.getTitle());
+        book.setAuthor(updatedBook.getAuthor());
+        book.setIsbn(updatedBook.getIsbn());
+        book.setPrice(updatedBook.getPrice());
+
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping
+    public List<Book> deleteById(@RequestParam long id) {
+        return bookRepository.deleteBookById(id);
     }
 }
--- library-api/src/main/java/com/flores/libraryapi/repository/BookRepository.java
+++ library-api/src/main/java/com/flores/libraryapi/repository/BookRepository.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface BookRepository extends MongoRepository<Book, String> {
 
     List<Book> findByAuthor(String author);
+    List<Book> findByTitle(String title);
+    Book findById(long id);
+    List<Book> deleteBookById(long id);
 }
